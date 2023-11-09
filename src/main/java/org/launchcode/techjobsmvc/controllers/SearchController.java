21e48b19f031bea2f43abfd026bc33ac9289ca85
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-11-08 20:02:58 -0600

    halfwayThroughTask3

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,19 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
+import org.springframework.core.annotation.MergedAnnotations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.naming.directory.SearchResult;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -15,6 +25,8 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 @RequestMapping("search")
 public class SearchController {
 
+    private static List<Job> searchResults = new ArrayList<>();
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
@@ -22,6 +34,20 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("/results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
 
+        searchResults = JobData.findByColumnAndValue(searchTerm, searchType);
+
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("searchResults", searchResults);
+
+        if (searchTerm = "all".toLowerCase()) {
+            return JobData.findAll();
+        } else {
+            return findByColumnAndValue;
+        }
+        return "redirect:/search/results";
+    }
 }
 
